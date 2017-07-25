2051d49a5c88b0c250168268bb95c34d36efcb69
Ian Walters <waltersi@ramsayhealth.com.au>
2017-07-25 12:42:37 +1000

    Add flow test

--- src/spec/basicSpec.ts
+++ src/spec/basicSpec.ts
@@ -1,5 +1,6 @@
 import 'jasmine';
-import { Soak } from '..';
+import { Soak, Flow } from '..';
+import * as Rx from 'rxjs';
 
 type CounterState = {
     counter: number;
@@ -16,9 +17,28 @@ type CounterAction = {
 const counterSoak: Soak<CounterState, CounterAction> = (state, action) =>
     ({ counter: state.counter + action.delta });
 
+const counterFlow: Flow<CounterAction> = (in$) =>
+    in$.map(a => ({delta: a.delta*2}));
+
 describe("soak modifies state", () => {
     const next = counterSoak(initialCounterState, { delta: 2});
     it('has changed', () => {
         expect(next.counter).toBe(2);
     })
 });
+
+describe("flow modifies actions", () => {
+    var deltaList: number[];
+    beforeEach(() => {
+        const subject$ = new Rx.Subject<CounterAction>();
+        const action$ = counterFlow(subject$);
+        deltaList = [];
+        action$.subscribe(a => deltaList.push(a.delta));
+        subject$.next({ delta: 3 });
+        subject$.complete();
+    });
+
+    it("Should only defined", () => expect(deltaList).toBeDefined());
+    it("should mutate only", () => expect(deltaList.length).toBe(1));
+    it("should change detla", () => expect(deltaList[0]).toBe(6));
+})
