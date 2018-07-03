369c1916d47d4699815b0dc2c53d9639179a8882
Ian Walters <ian@walters.id.au>
2018-07-03 21:10:56 +1000

    add tests for new helpers

--- src/Action.ts
+++ src/Action.ts
@@ -12,6 +12,42 @@ export type ActionCreatorMap<A> = {
 };
 
 /**
+ * The Union of an ActionCreatorMap for action type any.
+ * 
+ * This is used to define an Action type from a creator map.
+ */
+export type ActionCreatorMapUnion<A extends ActionCreatorMap<any>> = ReturnType<A[keyof A]>
+
+/**
+ * Creates an empty Action type.  Can be used as the basis in a spread operator.
+ * 
+ * e.g.
+ * enum ActionType {
+ *      Foo = 'FOO'
+ * }
+ * 
+ * const creators = {
+ *   foo: (a: number, b: string) => ({ ...createAction(ActionType.Foo), a, b })
+ * }
+ * 
+ * This is a convenience for;
+ * 
+ * const creators = {
+ *  foo: (a: number, b: string) => ({ ActionType.Foo as ActionType.Foo, a, b })}
+ * 
+ * @param type is the type value for the action. Must be of type string for this function.
+ */
+export const createAction = <T extends string>(type: T) => ({ type });
+
+/**
+ * 
+ * @param type is the type to test for
+ * @param func is the function that defines the return type for the action type.
+ */
+export const isAction = <T, F extends ActionCreator<any>>(type: T, func: F) =>
+    <A extends { type: T }>(a: A): a is ReturnType<F> => a.type === type
+
+/**
  * Takes a dispatch function and action creator function and returns a function that will
  * dispatch the action before returning it.
  * @param fn action creator function
---
+++ src/Acton.spec.ts
@@ -0,0 +1,42 @@
+import { createAction, isAction, ActionCreatorMapUnion } from './Action';
+
+export enum ActionType {
+    SignIn = 'SignIn',
+    SignOut = 'SignOut',
+}
+
+export namespace Action {
+    export const creators = {
+        signIn: () => createAction(ActionType.SignIn),
+        signOut: (next: number) => ({ ...createAction(ActionType.SignOut), next })
+    }
+
+    export const filters = {
+        isSignIn: isAction(ActionType.SignIn, creators.signIn),
+        isSignOut: isAction(ActionType.SignOut, creators.signOut)
+    }
+}
+
+type Action = ActionCreatorMapUnion<typeof Action.creators>;
+
+describe('createAction', () => {
+    it('it should narrow type property', () => {
+        let action = Action.creators.signOut(5);
+        expect(Action.filters.isSignOut(action)).toBe(true)
+        action.next == 5;
+    });
+});
+
+describe('isAction', () => {
+    it('should work on non-string types', () => {
+        let a = () => ({ type: 5 as 5 })
+        let b = () => ({ type: 6 as 6, next: 2 })
+        let isA = isAction(5, a);
+        let isB = isAction(6, b);
+
+        expect(isA(a())).toBe(true);
+        expect(isA(b())).toBe(false);
+        expect(isB(a())).toBe(false);
+        expect(isB(b())).toBe(true);
+    })
+})
